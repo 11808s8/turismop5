212438dce23c94145b1f0952b899ec0556a34d6d
Adriano Gomes <adrianogsss@gmail.com>
2018-12-02 10:54:11 -0200

    Adição de página de login e algumas verificações de user logado

--- turismo_p5/catalogo_guias/urls.py
+++ turismo_p5/catalogo_guias/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from djgeojson.views import GeoJSONLayerView
 from catalogo_guias.models import Ponto
 from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-    path('roteiro/new/', views.roteiro_new, name='roteiro_new'),
+    # path('roteiro/new/', views.roteiro_new, name='roteiro_new'),
     path('atrativo/new/', views.atrativo_new, name='atrativo_new'),
     path('ponto/new/', views.ponto_new, name='ponto_new'),
     path('dados_todos_pontos', GeoJSONLayerView.as_view(model=Ponto), name='data'),
@@ -14,4 +14,5 @@ urlpatterns = [
     path('dados_todos_atrativos',views.dados_todos_atrativos, name='dados_atrativos'),
     path('atrativo/<int:primary_key>', views.atrativo_detail_view, name='atrativo_detail'),
     path('atrativos/guia', views.atrativoGuiaAtrela, name='atrativo_guia_atrela'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
